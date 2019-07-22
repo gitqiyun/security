badf1c9d2df579ad7d8f7e77ae74a0fad632b93c
gitqiyun <1114260199@qq.com>
2019-07-22 09:39:34 +0800

    request session更进一步理解

--- src/main/java/com/security/util/SessionContextHolder.java
+++ src/main/java/com/security/util/SessionContextHolder.java
@@ -26,6 +26,14 @@ public class SessionContextHolder {
          * 原理过程：每个请求即一个线程进入应用，都会根据session等相关值封装成authentication保存到SecurityContext，再将
          * SecurityContext保存到SecurityContextHolder中的，相当一个map，当前线程作为主键，当前的SecurityContext作为值
          *
+         * 请求request进入系统，从request中获取session,从session获取上下文SecurityContext，如果没有登录则登录将用户信息封装为
+         * authentication，将SecurityContext保存到SecurityContextHolder中，相当一个map，当前线程作为主键，当前的SecurityContext作为值
+         * 如果登录了，即SecurityContext不为空，直接保存到SecurityContextHolder中，每次请求都会保存请求结束后会清空。
+         *
+         * HttpRequestResponseHolder holder = new HttpRequestResponseHolder(request,response);
+         * SecurityContext contextBeforeChainExecution = repo.loadContext(holder);
+         * Object contextFromSession = httpSession.getAttribute(springSecurityContextKey);
+         *
          * 详解
          * https://www.cnblogs.com/longfurcat/p/9417912.html
          */
